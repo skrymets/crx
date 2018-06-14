d421df4b7a3ee420352239029c696d9beefafa47
Sergii Krymets <skrymets@gmail.com>
2018-06-14 09:48:02 +0300

    Fix integration test harnes for the existing REST endpoints

--- crx-web/src/test/java/com/crxmarkets/arquillian/api/CalculatorResourceTest.java
+++ crx-web/src/test/java/com/crxmarkets/arquillian/api/CalculatorResourceTest.java
@@ -15,20 +15,31 @@
  */
 package com.crxmarkets.arquillian.api;
 
+import com.crxmarkets.alg.rain.Calculator2;
 import com.crxmarkets.api.CalculatorResourceImpl;
 import com.crxmarkets.dao.HistoryEntityService;
 import com.crxmarkets.services.VolumeCalculatorServiceLocal;
+import com.crxmarkets.web.client.shared.CalculationResult;
+import com.crxmarkets.web.client.shared.CalculationTask;
 import com.crxmarkets.web.client.shared.CalculatorResource;
+import com.crxmarkets.web.client.shared.HistoryItem;
 import java.net.URL;
+import static java.util.Arrays.asList;
+import java.util.Date;
+import java.util.List;
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -55,11 +66,13 @@ public class CalculatorResourceTest {
                 .addPackage(CalculatorResource.class.getPackage())
                 .addPackage(CalculatorResourceImpl.class.getPackage())
                 .addPackage(HistoryEntityService.class.getPackage())
+                .addPackage(Calculator2.class.getPackage())
                 .addAsDirectory("WEB-INF/lib")
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml")
                 .addAsResource("log4j.properties", "log4j.properties")
                 .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
-                .addAsWebInfResource("jboss-deployment-structure.xml");
+                .addAsWebInfResource("test-beans.xml", "beans.xml")
+                .addAsWebInfResource("test-jboss-deployment-structure.xml", "jboss-deployment-structure.xml");
 
         LOG.info(war.toString(true));
         return war;
@@ -77,32 +90,57 @@ public class CalculatorResourceTest {
 
     @Test
     public void testSimpleCalculateTask() {
-//
-//        Client client = ClientBuilder.newClient();
-//        WebTarget target = client.target(deploymentURL.toString() + "rest/calculator/task");
-//
-//        CalculationTask task = new CalculationTask();
-//        task.setHeights(asList(new Integer[]{3, 2, 1, 4}));
-//        System.out.println(task.toString());
-//
-//        CalculationResult response = target.request(MediaType.APPLICATION_JSON)
-//                .post(Entity.json(task), CalculationResult.class);
-//
-//        List<Integer> expectedResult = asList(new Integer[]{3, 3, 3, 4});
-//
-//        assertNotNull(response);
-//        assertTrue(response.getTotalVolume() == 3);
-//        assertEquals(response.getLevels(), expectedResult);
+
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(deploymentURL.toString() + "rest/calculator/task");
+
+        CalculationTask task = new CalculationTask();
+        task.setHeights(asList(new Integer[]{3, 2, 1, 4}));
+        System.out.println(task.toString());
+
+        CalculationResult response = target.request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(task), CalculationResult.class);
+
+        List<Integer> expectedResult = asList(new Integer[]{3, 3, 3, 4});
+
+        assertNotNull(response);
+        assertTrue(response.getTotalVolume() == 3);
+        assertEquals(response.getLevels(), expectedResult);
     }
 
-//    @Test
-//    public void testFailedCalculateTask() {
-//    }
-//
-//    @Test
-//    public void testCreateCorrectHistoryItem() {
-//    }
-//
+    @Test(expected = BadRequestException.class)
+    public void testFailedCalculateTask() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(deploymentURL.toString() + "rest/calculator/task");
+
+        CalculationTask task = new CalculationTask();
+        task.setHeights(null);
+
+        CalculationResult response;
+        response = target.request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(task), CalculationResult.class);
+
+        fail("BadRequestException was expected above");
+    }
+
+    @Test
+    public void testCreateCorrectHistoryItem() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(deploymentURL.toString() + "rest/calculator/history");
+
+        HistoryItem hi = new HistoryItem();
+        hi.setDateTime(new Date());
+        hi.setTask("3 2 1 4");
+        hi.setCalculation("3 3 3 4");
+        hi.setTotal(3);
+
+        Integer response = target.request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(hi), Integer.class);
+
+        assertNotNull(response);
+        System.out.println("New item ID: " + String.valueOf(response));
+    }
+
 //    @Test
 //    public void testCreateInvalidHistoryItem() {
 //    }
@@ -122,5 +160,4 @@ public class CalculatorResourceTest {
 //    @Test
 //    public void testCleanupAllHistoryData() {
 //    }
-
 }
